bc153c7058d43f20a3a1c47059df1802e45600de
Vili Sahramaa <vili.sahramaa@gmail.com>
2019-02-28 12:11:49 +0200

    Updates

---
+++ precache-manifest.904328ae2df75f5206e51535ecc6f0ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e20ffba00176dbae8a71",
+    "url": "/Reactris/static/js/runtime~main.e20ffba0.js"
+  },
+  {
+    "revision": "d5617cb699ef30637031",
+    "url": "/Reactris/static/js/main.d5617cb6.chunk.js"
+  },
+  {
+    "revision": "d6cbbdcb9ddb5bc94f2a",
+    "url": "/Reactris/static/js/2.d6cbbdcb.chunk.js"
+  },
+  {
+    "revision": "d5617cb699ef30637031",
+    "url": "/Reactris/static/css/main.11940c8c.chunk.css"
+  },
+  {
+    "revision": "9e14530c29f5d9f8149076588f7d22b5",
+    "url": "/Reactris/index.html"
+  }
+];
