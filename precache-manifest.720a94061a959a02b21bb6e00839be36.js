40e1cb40ce8b4441b0957b15c2fe9a584b8e4c13
Vili Sahramaa <vili.sahramaa@gmail.com>
2019-03-06 15:01:50 +0200

    Updates

---
+++ precache-manifest.720a94061a959a02b21bb6e00839be36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "21076a493aa22179d51d",
+    "url": "/reactris/static/js/runtime~main.21076a49.js"
+  },
+  {
+    "revision": "0d81708f9b2fdb32d5e8",
+    "url": "/reactris/static/js/main.0d81708f.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/reactris/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "0d81708f9b2fdb32d5e8",
+    "url": "/reactris/static/css/main.0b5b58ac.chunk.css"
+  },
+  {
+    "revision": "fd27e83226217778173e968b9fb4d927",
+    "url": "/reactris/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Reactris/precache-manifest.1a5900f61531b673f9e7e3782fd2558f.js"
+  "/reactris/precache-manifest.720a94061a959a02b21bb6e00839be36.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Reactris/index.html", {
+workbox.routing.registerNavigationRoute("/reactris/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
