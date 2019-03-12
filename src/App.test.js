3fd5ebf16931d423e4b68b9ddeebd1ded4b8981d
Vili Sahramaa <vili.sahramaa@gmail.com>
2019-03-12 12:36:34 +0200

    fixed test, added missing keys

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Reactris from './Reactris';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Reactris />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/Components/BoardRenderer.js
+++ src/Components/BoardRenderer.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 
 class BoardRenderer extends Component {
 
-  drawCol(value){
+  drawCol(value, index){
     if (value === 0) {
-      return <div className="col empty"></div>;
+      return <div className="col empty" key={index}></div>;
     }
-    return <div className="col filled"></div>;
+    return <div className="col filled" key={index}></div>;
   }
 
   render(){
     return(
       <div className="table">
-        {this.props.state.map(row => (
-          <div className="row" key={row.id}>
+        {this.props.state.map((row, index) => (
+          <div className="row" key={index}>
           
-            {row.map(col => (
-              this.drawCol(col)
+            {row.map((col, index) => (
+              this.drawCol(col, index)
             ))}
 
           </div>
